ac3aa4d682c498b8c3bdfe49664985acc1bde287
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-02-26 20:17:52 +0100

    RemoveException class started

---
+++ src/main/java/mappe/del1/hospital/exception/RemoveException.java
@@ -0,0 +1,9 @@
+package mappe.del1.hospital.exception;
+
+public class RemoveException extends Exception{
+    public static final long serialVersionUID=1L;
+
+    public RemoveException(String message) {
+        super(message);
+    }
+}
