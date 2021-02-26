12884eeba2dde68eb9e22c7976d602dcf836585f
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-02-26 17:53:59 +0100

    Person class implemented

---
+++ src/main/java/mappe/del1/hospital/Person.java
@@ -0,0 +1,50 @@
+package mappe.del1.hospital;
+
+public abstract class Person {
+    private String firstName;
+    private  String lastName;
+    private String socialSecurityNumber;
+
+    public Person(String firstName, String lastName, String socialSecurityNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSocialSecurityNumber(socialSecurityNumber);
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullname(){
+        return firstName+" "+lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                '}';
+    }
+}
