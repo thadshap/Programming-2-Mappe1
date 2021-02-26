00569f2149bf8ab2ee0bf4ea645b8fd547b91130
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-02-26 17:56:26 +0100

    Employee class implemented

---
+++ src/main/java/mappe/del1/hospital/Employee.java
@@ -0,0 +1,12 @@
+package mappe.del1.hospital;
+
+public class Employee extends Person {
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
