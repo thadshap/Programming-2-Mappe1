0c1218979750a0f63e0eb89b393220f9f57f9f0a
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-02-26 17:57:19 +0100

    Nurse class implemented

---
+++ src/main/java/mappe/del1/hospital/healthpersonal/Nurse.java
@@ -0,0 +1,15 @@
+package mappe.del1.hospital.healthpersonal;
+
+import mappe.del1.hospital.Employee;
+
+public class Nurse extends Employee {
+
+    public Nurse(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
