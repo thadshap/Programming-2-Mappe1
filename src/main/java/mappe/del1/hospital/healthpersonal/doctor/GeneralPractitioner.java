d61149819fe0faea27af54758c281197a3452f26
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:45:04 +0100

    GeneralPractitioner java doc written

--- src/main/java/mappe/del1/hospital/healthpersonal/doctor/GeneralPractitioner.java
+++ src/main/java/mappe/del1/hospital/healthpersonal/doctor/GeneralPractitioner.java
@@ -1,13 +1,30 @@
 package mappe.del1.hospital.healthpersonal.doctor;
 
+/**
+ * GeneralPractitioner extends Doctor class and has a mutator method.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 import mappe.del1.hospital.Patient;
 
 public class GeneralPractitioner extends Doctor{
-
+    /**
+     * Constructor
+     * @param firstName (String)
+     * @param lastName (String)
+     * @param socialSecurityNumber (String)
+     */
     public GeneralPractitioner(String firstName, String lastName, String socialSecurityNumber) {
         super(firstName, lastName, socialSecurityNumber);
     }
 
+    /**
+     * setDiagnosis is an abstract methode for setting a patient's diagnosis
+     * @param patient (patient object)
+     * @param diagnosis (string)
+     */
     @Override
     public void setDiagnosis(Patient patient, String diagnosis) {
 
