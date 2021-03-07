c597f46dd2bdf195340f432c7a4f75fe6c8af80e
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:44:42 +0100

    Doctor java doc written

--- src/main/java/mappe/del1/hospital/healthpersonal/doctor/Doctor.java
+++ src/main/java/mappe/del1/hospital/healthpersonal/doctor/Doctor.java
@@ -1,12 +1,30 @@
 package mappe.del1.hospital.healthpersonal.doctor;
 
+/**
+ * Doctor is an abstract class that extends employee and is a "form" to register doctors.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 import mappe.del1.hospital.Employee;
 import mappe.del1.hospital.Patient;
 
 public abstract class Doctor extends Employee {
+    /**
+     * Constructor
+     * @param firstName (string)
+     * @param lastName (string)
+     * @param socialSecurityNumber (string)
+     */
     public Doctor(String firstName, String lastName, String socialSecurityNumber) {
         super(firstName, lastName, socialSecurityNumber);
     }
 
+    /**
+     * setDiagnosis is an abstract methode for setting a patient's diagnosis
+     * @param patient (patient object)
+     * @param diagnosis (string)
+     */
     public abstract void setDiagnosis (Patient patient,String diagnosis);
 }
