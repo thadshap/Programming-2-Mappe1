2492f6573499a5deb7dd851b504872c17b1f578c
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:46:22 +0100

    RemoveException java doc written

--- src/main/java/mappe/del1/hospital/exception/RemoveException.java
+++ src/main/java/mappe/del1/hospital/exception/RemoveException.java
@@ -1,8 +1,18 @@
 package mappe.del1.hospital.exception;
 
+/**
+ * RemoveException is a exception class.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 public class RemoveException extends Exception{
+
+    //serialVersionUID declaration
     public static final long serialVersionUID=1L;
 
+    //constructor
     public RemoveException(String message) {
         super(message);
     }
