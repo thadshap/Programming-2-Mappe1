00f9d4c2d80f9feea74cabfbb319e53b68824ef8
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:45:39 +0100

    Patient java doc written

--- src/main/java/mappe/del1/hospital/Patient.java
+++ src/main/java/mappe/del1/hospital/Patient.java
@@ -1,28 +1,54 @@
 package mappe.del1.hospital;
 
+/**
+ * Patient extends Person class and implements diagnosis. This class has some access and mutator methods.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 public class Patient extends Person implements Diagnosable {
     private String diagnosis = "";
 
+    /**
+     * Constructor
+     * @param firstName (String)
+     * @param lastName (String)
+     * @param socialSecurityNumber (String)
+     */
     public Patient(String firstName, String lastName, String socialSecurityNumber) {
         super(firstName, lastName, socialSecurityNumber);
     }
 
+    /**
+     * (access methode)
+     * @return diagnosis
+     */
     public String getDiagnosis() {
         return diagnosis;
     }
 
+    /**
+     * setting diagnosis to a patient (mutator methode)
+     * @param diagnosis (String)
+     */
     public void setDiagnosis(String diagnosis) {
         this.diagnosis = diagnosis;
     }
 
+    /**
+     * toString methode
+     * @return Patient{ firstName='firstName'
+                        lastName='lastName'
+                        diagnosis='diagnosis'
+                      }
+     */
     @Override
     public String toString() {
         return "Patient{" +
+                "firstName='" + getFirstName() + '\'' +
+                "lastName='" + getLastName() + '\'' +
                 "diagnosis='" + diagnosis + '\'' +
                 '}';
-    }
-
-    public void setDiagnosis() {
-
     }
 }
