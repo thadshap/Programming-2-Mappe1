34517f5469a1bb59f4dcacecc5247f13266a030a
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:45:29 +0100

    Nurse java doc written

--- src/main/java/mappe/del1/hospital/healthpersonal/Nurse.java
+++ src/main/java/mappe/del1/hospital/healthpersonal/Nurse.java
@@ -1,13 +1,31 @@
 package mappe.del1.hospital.healthpersonal;
 
+/**
+ * Nurse extends Employee class.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 import mappe.del1.hospital.Employee;
 
 public class Nurse extends Employee {
-
+    /**
+     * Constructor
+     * @param firstName (String)
+     * @param lastName (String)
+     * @param socialSecurityNumber (String)
+     */
     public Nurse(String firstName, String lastName, String socialSecurityNumber) {
         super(firstName, lastName, socialSecurityNumber);
     }
 
+    /**
+     * toString methode
+     * @return firstName='firstName'
+               lastName='lastName'
+               socialSecurityNumber='socialSecurityNumber'
+     */
     @Override
     public String toString() {
         return super.toString();
