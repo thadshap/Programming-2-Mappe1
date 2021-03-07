20b8d64a46bf9debb5fbed95479b0dde34082bfd
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:44:29 +0100

    Diagnosable java doc written

--- src/main/java/mappe/del1/hospital/Diagnosable.java
+++ src/main/java/mappe/del1/hospital/Diagnosable.java
@@ -1,5 +1,16 @@
 package mappe.del1.hospital;
 
+/**
+ * Diagnosable is an interface class and has a mutator methode.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 public interface Diagnosable {
-    public void setDiagnosis();
+    /**
+     * (access methode)
+     * @param diagnosis (String)
+     */
+    public void setDiagnosis(String diagnosis);
 }
