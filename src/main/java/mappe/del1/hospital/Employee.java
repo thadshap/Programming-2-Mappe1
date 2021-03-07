d18601ec8627c93d6bc50398140675435df1d373
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2021-03-07 01:44:50 +0100

    Employee java doc written

--- src/main/java/mappe/del1/hospital/Employee.java
+++ src/main/java/mappe/del1/hospital/Employee.java
@@ -1,10 +1,29 @@
 package mappe.del1.hospital;
 
+/**
+ * RemoveException is a exception class.
+ *
+ * @version 11.03.2021
+ * @author Thadshajini
+ */
+
 public class Employee extends Person {
+    /**
+     * Constructor
+     * @param firstName (String)
+     * @param lastName (String)
+     * @param socialSecurityNumber (String)
+     */
     public Employee(String firstName, String lastName, String socialSecurityNumber) {
         super(firstName, lastName, socialSecurityNumber);
     }
 
+    /**
+     * toString methode
+     * @return firstName = 'firstName'
+               lastName = 'lastName'
+               socialSecurityNumber = 'socialSecurityNumber'
+     */
     @Override
     public String toString() {
         return super.toString();
